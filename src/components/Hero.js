ba852c3bba207d4d44e5da88e2e8c9465ca11b09
sahilkakwani9 <sahkakwani@gmail.com>
2022-07-16 18:54:01 +0530

    fea: hero added

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { SpotifyLogoFull } from "../Assets/Icons";
+import logo from "../Assets/DeBlog-logos_black.png"
 const Hero = ({ connectWallet }) => {
     useEffect(() => {
         window.addEventListener("scroll", () => {
@@ -22,17 +23,22 @@ const Hero = ({ connectWallet }) => {
     return (
         <div className="min-h-screen glass min-w-full">
             <nav id="nav" className="flex flex-row justify-around sticky top-0 p-5">
-                <SpotifyLogoFull id="svg" />
-                <ul className="flex flex-row items-center justify-center transition-all ease-in-out duration-300 text-2xl ">
-                    <li className="mx-6 my-2 hover:-translate-y-1  text-green-400 font-semibold " >Home</li>
-                    <li className="mx-6 my-2 hover:-translate-y-1 text-green-400 font-semibold " >Listen</li>
-                    <li className="mx-6 my-2 hover:-translate-y-1 text-green-400 font-semibold " >About Us</li>
+                {/* <SpotifyLogoFull id="svg" /> */}
+                <div className="left-0">
+                <img className="absolute -top-20 left-0 w-1/6" src={logo} alt="" />
+                </div>
+                <ul className="flex flex-row items-center justify-center transition-all ease-in-out duration-300 text-2xl">
+                    <li className="mx-12 my-2 hover:-translate-y-1  font-semibold " >Home</li>
+                    <li className="mx-6 my-2 hover:-translate-y-1 font-semibold " >Listen</li>
+                    <li className="mx-6 my-2 hover:-translate-y-1 font-semibold " >About Us</li>
                 </ul>
             </nav>
-            <main className="min-h-screen bg-gradient-to-b flex flex-col items-center justify-center overflow-hidden rounded-lg">
-                <h2 className="font-extrabold text-8xl gradient sm:text-6xl">Explore Decentrlized Spotify</h2>
-                <h2 className="text-5xl   text-green-400 my-10 font-semibold">Listen Own Earn</h2>
-                <button className="bg-gradient-to-r from-emerald-500 to-lime-600 px-6 py-3 rounded-xl hover:scale-94 text-2xl text-white" onClick={connectWallet}>Connect Wallet </button>
+            <main className="min-h-screen -mt-8 bg-gradient-to-b flex flex-col items-center justify-center overflow-hidden rounded-lg">
+                <h2 className="font-Two text-8xl w-1/2 text-center">Your blogs go <span className="text-transparent bg-clip-text bg-gradient-to-r from-green-400 via-blue-500 to-purple-600">Decentralised</span></h2>
+                {/* <img src={logo} className='w-1/2 -mt-96' alt="" /> */}
+
+                <p className="text-2xl text-center w-1/2 my-10">DeBlog helps you create blogs and store them on a decentralised blockchain within a span of seconds.</p>
+                <button className="mt-8 bg-gradient-to-r from-blue-600 to-purple-600 px-6 py-5 rounded-2xl hover:scale-94 text-2xl text-white font-Two" onClick={connectWallet}>Connect Wallet </button>
             </main>
             <section className="min-h-screen glass mx-2 rounded-xl flex flex-row items-center justify-around">
                 <main className="rounded-full h-72  w-72   gap-3 glass border-0 border-green-200  flex justify-center items-center p-2 ">
