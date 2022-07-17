0c5fdb4fc90e03d4628012d4ce8b35541822c079
sahilkakwani9 <sahkakwani@gmail.com>
2022-07-17 17:48:41 +0530

    fea: made blogpage dynamic

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,16 +1,16 @@
 import useStore from "../Store/store";
 
 export default function BlogCard({ blogdata, index }) {
-  console.log(blogdata);
+  // console.log(blogdata);
   const store = useStore();
   const currentAccount = store.currentAccount.slice(38);
   return (
     <>
       <div className='MusicCard w-full h-72 bg-gradient-to-r from-sky-400 to-sky-300 flex flex-row items-center  justify-center rounded-xl p-4 overflow-hidden hover:bg-[hsla(0,0%,100%,.1)] hover:transition-all hover:delay-75'>
         <div className='h-full pt-4 pr-8 text-black flex-1 flex flex-col justify-start items-start'>
-          <div className="self-start text-4xl">{blogdata.blogTitle}</div>
-          <div className='text-2xl my-2'>{blogdata.subTitle}</div>
-          <h4 className='text-xl'>By 0x...{blogdata.blogOwner} *  2 min read</h4>
+          <div className="self-start text-4xl font-One">{blogdata.blogTitle}</div>
+          <div className='text-2xl my-2 font-Three'>{blogdata.subTitle}</div>
+          <h4 className='text-xl text-[#4b4b4b]' >By 0x...{blogdata.blogOwner} *  2 min read</h4>
           {/* <p className='text-xl my-4 w-5/6 text-base'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore maiores voluptas, eos iusto reprehenderit quae non sint est, voluptatem obcaecati hic excepturi? Distinctio aut corporis quisquam eos vel, eveniet consequuntur.</p> */}
         </div>
         <div className='w-40 h-40 rounded-xl '>
